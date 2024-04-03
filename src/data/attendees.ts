f70fbe6b492beff57604a005ea244f53eda6b310
Gabriel Bertaglia <gaabrielcursi@gmail.com>
2024-04-03 14:48:58 -0300

    ok

--- src/data/attendees.ts
+++ src/data/attendees.ts
@@ -2,10 +2,10 @@ import { faker } from "@faker-js/faker";
 
 export const attendees = Array.from({ length: 200 }).map(() => {
   return {
-    id: faker.number.int({ min: 10000, max: 20000 }),
+    id: String(faker.number.int({ min: 10000, max: 20000 })),
     name: faker.person.fullName(),
     email: faker.internet.email().toLocaleLowerCase(),
-    createdAt: faker.date.recent({ days: 30 }),
-    checkedInAt: faker.date.recent({ days: 7 }),
+    createdAt: String(faker.date.recent({ days: 30 })),
+    checkedInAt: String(faker.date.recent({ days: 7 })),
   };
 });
